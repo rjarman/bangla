8e7ad021e23557519c3473f4d7620d264c38f5a9
Rafsun Jany Arman <armanrafsunjany@gmail.com>
2020-08-02 23:12:29 +0600

    BanglaWords.py and some strutural changes

--- src/bangla/BanglaWords.py
+++ src/bangla/BanglaWords.py
@@ -1,2 +1,7 @@
-class Words():
-    pass
+from .Generel import Files
+class Words(Files):
+    def __init__(self, _data_type, _type = 'json', _keys = 'all'):
+        Files.__init__(self, _type = _type, _data_type = _data_type, _keys = _keys)
+
+    def get(self):
+        return self.get_data()
--- src/setup.py
+++ src/setup.py
@@ -5,5 +5,6 @@ setup(
     version = '0.1',
     description = 'This is a package consist of bengali words, stop words, stemmer module, etc.',
     packages = ['bangla'],
-    zip_safe = False
+    zip_safe = False,
+    include_package_data = True
     )
