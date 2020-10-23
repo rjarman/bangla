6b1647952a9a367082f0e07dc6a77c40b7bb4a2a
Rafsun Jany Arman <armanrafsunjany@gmail.com>
2020-10-23 15:06:48 +0600

    add Linux compatibility

--- src/setup.py
+++ src/setup.py
@@ -5,6 +5,7 @@ setup(
     version = '1',
     description = 'This is a package consist of bengali words, stop words, stemmer module, etc.',
     packages = ['bangla'],
+    package_data={'bangla': ['data/*.pkl', 'lib/*.py']},
     zip_safe = False,
     include_package_data = True
     )
